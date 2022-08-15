b19a42e4fe4e4d29b81ca2bdf41a3e2ee1c78ab3
Recep Gumus <recepgumusyasin@gmail.com>
2022-08-15 18:28:21 +0300

    job create Vue.js 2 homepage
    Took 44 minutes

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,7 @@ import 'vue-form-wizard/dist/vue-form-wizard.min.css'
 Vue.use(ElementUI,{locale});
 Vue.use(VueFormWizard);
 Vue.component('job-create', require('./components/ExampleComponent.vue').default)
+Vue.component('job-filter', require('./components/JobFilter.vue').default)
 
 const app = new Vue({
     el: '#app',
---
+++ resources/js/components/JobFilter.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <section >
+        <h1>Test</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+    },
+    mounted() {
+        alert('a')
+    },
+}
+</script>
